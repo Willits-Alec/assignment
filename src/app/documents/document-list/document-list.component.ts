75a5f08daa26ee27b29d8c3835b1a78b7a5bc2bf
alec willits <alecwillits@gmail.com>
2024-02-01 18:29:14 -0700

    added document component and supporting files.

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter();
+
+  documents = [
+    new Document('1', 'PlaceHolder 1', 'This is the placeholder doc 1.', 'https://www.byui.edu/1'),
+    new Document('2', 'PlaceHolder 2', 'This is the placeholder doc 2.', 'https://www.byui.edu/2'),
+    new Document('3', 'PlaceHolder 3', 'This is the placeholder doc 3.', 'https://www.byui.edu/3'),
+    new Document('4', 'PlaceHolder 4', 'This is the placeholder doc 4.', 'https://www.byui.edu/4')
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
+
+
 
 }
