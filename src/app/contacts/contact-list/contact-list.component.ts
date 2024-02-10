dd5a212036d107277ac50652b1ce9663aae32dbd
alec willits <alecwillits@gmail.com>
2024-02-09 19:46:19 -0700

    new contacts

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-list',
@@ -8,14 +9,20 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent {
   contacts: Contact[] = [
-    new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "../../assets/image/jacksonk.jpg", null),
-    new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/image/barzeer.jpg", null)
+    // new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "../../assets/image/jacksonk.jpg", null),
+    // new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/image/barzeer.jpg", null)
   ];
 
   @Output() selectedContactEvent = new EventEmitter<Contact>();
 
   onSelected(contact: Contact) {
     this.selectedContactEvent.emit(contact);
+  }
+
+  constructor(private contactService: ContactsService) {}
+
+  ngOnInit(): void {
+    this.contacts = this.contactService.getContacts();
   }
 
 }
